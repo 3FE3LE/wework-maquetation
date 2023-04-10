80cfd057f8b24543386d7b19045d883f53f1bb6d
Frank Licona(3FE3LE) <3l33f3@gmail.com>
2023-04-10 01:31:21 -0500

    add alert component

---
+++ pages/components/Alert.jsx
@@ -0,0 +1,20 @@
+
+import { useState } from 'react'
+
+import Link from 'next/link'
+export const Alert = () => {
+
+  const [closeAlert, setCloseAlert] = useState(false)
+  return (
+    <>
+      {!closeAlert && <div className='alert'>
+        <Link href={'/'}>
+          <div className='alert__container'>
+            <button onClick={() => setCloseAlert(true)} className='alert__close-button' >x</button>
+            <span className='alert__info'>Accede a espacios de trabajo cerca de ti con un descuento del 25 % en la cuota de membresía mensual de WeWork All Access Plus durante 5 meses. Se aplican términos y condiciones.</span><span className='alert__info--link'>Obtén más información</span>
+          </div>
+        </Link>
+      </div>}
+    </>
+  )
+}
