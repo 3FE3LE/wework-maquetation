76e181ab146d7add9a03411b165ba69cf18d4c97
Frank Licona(3FE3LE) <3l33f3@gmail.com>
2023-04-10 01:31:50 -0500

    add navbar component

---
+++ pages/components/Navbar.jsx
@@ -0,0 +1,31 @@
+import Link from 'next/link'
+import WeWorkLogo from '../../public/wework-logo.svg'
+
+export const Navbar = () => {
+  return (
+    <nav className='navbar'>
+          <div className='navbar__container'>
+            <div className='navbar__section'>
+              <Link href={'/'} >
+                <WeWorkLogo />
+              </Link >
+              <div className='navbar__item-list'>
+                <Link href={'/'} className='navbar__link hover-underline-animation'>
+                  Encuentra una ubicacion
+                </Link>
+                <Link href={'/'} className='navbar__link hover-underline-animation'>
+                  Soluciones
+                </Link>
+                <Link href={'/'} className='navbar__link hover-underline-animation'>
+                  Grandes Empresas
+                </Link>
+              </div>
+            </div>
+            <div className='navbar__section--seccond'>
+              <Link href={'/'} className='navbar__button'>Contacto</Link>
+              <Link href={'/'} className='navbar__link hover-underline-animation'>Inicio de sesion de mienbros</Link>
+            </div>
+          </div>
+        </nav>
+  )
+}
