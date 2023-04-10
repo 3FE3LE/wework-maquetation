c8dd28016954e3d914de0bf86911459c3585ee28
Frank Licona(3FE3LE) <3l33f3@gmail.com>
2023-04-10 02:14:39 -0500

    fix undestructuring img property of options array

--- pages/components/LookingFor/LookingForCard.jsx
+++ pages/components/LookingFor/LookingForCard.jsx
@@ -4,7 +4,7 @@ const LookingForCard = ({option}) => {
   const {img, description, title} = option
   return (
     <div className='looking-for__card'>
-      <Image height={100} width={100} alt={title} className='looking-for__card__img' src={img} />
+      {img}
       <div>
         <p>{title}</p>
         <span className='looking-for__description'>{description}</span>
--- pages/components/LookingFor/index.jsx
+++ pages/components/LookingFor/index.jsx
@@ -9,25 +9,25 @@ const index = () => {
 
   const options = [
     {
-      img: img1,
+      img: <Image height={100} width={100} alt={title} className='looking-for__card__img' src={img1} />,
       title: 'Membresía de coworking',
       description: 'Hot desks, salas, cabinas telefónicas y más',
       peopleCapacity: 'Mas de 1'
     },
     {
-      img: img2,
+      img: <Image height={100} width={100} alt={title} className='looking-for__card__img' src={img2} />,
       title: 'Oficina Privada',
       description: 'Oficinas listas cona menidades compartidas',
       peopleCapacity: '1-20+'
     },
     {
-      img: img3,
+      img: <Image height={100} width={100} alt={title} className='looking-for__card__img' src={img3} />,
       title: 'Excecutive Office',
       description: 'Oficinas de primer nivel con amenidades privadas',
       peopleCapacity: '1-100'
     },
     {
-      img: img4,
+      img: <Image height={100} width={100} alt={title} className='looking-for__card__img' src={img4} />,
       title: 'Sala de reuniones',
       description: 'Salas privadas que se pueden reservar por hora',
       peopleCapacity: '1-20'
