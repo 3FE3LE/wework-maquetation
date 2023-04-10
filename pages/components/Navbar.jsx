101bc464f1e06a33ea2f3a72643cf2da543ce7a9
Frank Licona(3FE3LE) <3l33f3@gmail.com>
2023-04-10 04:02:28 -0500

    add responsive desing, alert and navbar integrated

--- pages/components/Navbar.jsx
+++ pages/components/Navbar.jsx
@@ -1,32 +1,59 @@
 import Link from 'next/link'
 import WeWorkLogo from '../../public/wework-logo.svg'
+import { useState } from 'react'
+import Image from 'next/image'
+import menuIcon from '../../public/menu-icon.png'
+
 
 const Navbar = () => {
+  const [showMenu, setShowMenu] = useState(false)
   return (
     <nav className='navbar'>
-          <div className='navbar__container'>
-            <div className='navbar__section'>
-              <Link href={'/'} >
-                <WeWorkLogo />
-              </Link >
-              <div className='navbar__item-list'>
-                <Link href={'/'} className='navbar__link hover-underline-animation'>
-                  Encuentra una ubicacion
-                </Link>
-                <Link href={'/'} className='navbar__link hover-underline-animation'>
-                  Soluciones
-                </Link>
-                <Link href={'/'} className='navbar__link hover-underline-animation'>
-                  Grandes Empresas
-                </Link>
-              </div>
-            </div>
-            <div className='navbar__section--seccond'>
-              <Link href={'/'} className='navbar__button'>Contacto</Link>
-              <Link href={'/'} className='navbar__link hover-underline-animation'>Inicio de sesion de mienbros</Link>
-            </div>
+      <div className='navbar__container'>
+        <div className='navbar__section'>
+          <Image onClick={() => setShowMenu(true)} className='navbar__menu-toggler' height={25} width={25} alt='menu' src={menuIcon} />
+          {showMenu && <div className='navbar__mobile-menu'>
+            <span onClick={() => setShowMenu(false)} className='navbar__mobile-item'>X</span>
+            <Link href={'/'} className='navbar__mobile-item'>
+              Inicio
+            </Link>
+            <Link href={'/'} className='navbar__mobile-item'>
+              Encuentra una ubicacion
+            </Link>
+            <Link href={'/'} className='navbar__mobile-item'>
+              Soluciones
+            </Link>
+            <Link href={'/'} className='navbar__mobile-item'>
+              Grandes Empresas
+            </Link>
+            <Link href={'/'} className='navbar__mobile-item'>
+              Inicio de sesion de miembros
+            </Link>
+            <Link href={'/'}>
+              ¿Tienes preguntas o necesitas ayuda? +5715087567
+            </Link>
+          </div>}
+          <Link href={'/'} >
+            <WeWorkLogo />
+          </Link >
+          <div className='navbar__item-list'>
+            <Link href={'/'} className='navbar__link hover-underline-animation'>
+              Encuentra una ubicacion
+            </Link>
+            <Link href={'/'} className='navbar__link hover-underline-animation'>
+              Soluciones
+            </Link>
+            <Link href={'/'} className='navbar__link hover-underline-animation'>
+              Grandes Empresas
+            </Link>
           </div>
-        </nav>
+        </div>
+        <div className='navbar__section--seccond'>
+          <Link href={'/'} className='navbar__button'>Contacto</Link>
+          <Link href={'/'} className='navbar__link hover-underline-animation'>Inicio de sesion de miembros</Link>
+        </div>
+      </div>
+    </nav>
   )
 }
 export default Navbar
