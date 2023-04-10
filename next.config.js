e6de354e5792064afd9f65c99b98d3b178670551
Frank Licona(3FE3LE) <3l33f3@gmail.com>
2023-04-10 01:28:51 -0500

    clean project and and install libraries and set .jsx

--- next.config.js
+++ next.config.js
@@ -1,6 +1,19 @@
 /** @type {import('next').NextConfig} */
+const path = require('path');
 const nextConfig = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   reactStrictMode: true,
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"]
+    });
+
+    return config;
+  }
 }
 
 module.exports = nextConfig
+
---
+++ pages/index.jsx
@@ -0,0 +1,30 @@
+import Head from 'next/head'
+import { Alert } from './components/Alert'
+import { LookingFor } from './components/LookingFor'
+import { Navbar } from './components/Navbar'
+import { Promotions } from './components/Promotions'
+import { SelectLocation } from './components/SelectLocation'
+import { Solutions } from './components/Solutions'
+import { ConctactForm } from './components/ContactForm'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>WeWork | Soluciones de espacio de trabajo y oficina</title>
+        <meta name="description" content="" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="https://www.wework.com/vanilla-assets/images/we_logo.a32545795fe3f59def511d1b8069c271.svg" />
+      </Head>
+      <Alert />
+      <Navbar />
+      <main>
+        <SelectLocation />
+        <LookingFor />
+        <Promotions />
+        <Solutions />
+        <ConctactForm/>
+      </main>
+    </>
+  )
+}
